cc1ac901f051e89ca838ac3e32b94b37af6b1167
Pedro Contipelli <Pcontipelli10@hotmail.com>
2025-12-01 01:21:20 -0800

    Update template

--- dayX_solve.py
+++ dayX_solve.py
@@ -1,4 +1,6 @@
-input_file = open("dayX_input.txt", "r")
+import sys
+input = "input" if sys.argv[-1] != "--test" else "testcase"
+input_file = open(f"day1_{input}.txt", "r")
 
 lines = [line.strip() for line in input_file.readlines()]
 grid = [list(line.strip()) for line in input_file.readlines()]
