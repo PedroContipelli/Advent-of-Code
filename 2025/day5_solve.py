f4291b14a875599d23af3e96648b4f2b096dc516
Pedro Contipelli <Pcontipelli10@hotmail.com>
2025-12-04 22:59:49 -0800

    2025 (Day 5)

---
+++ 2025/day5_solve.py
@@ -0,0 +1,49 @@
+import sys, os
+input = "testcase" if sys.argv[-1] == "--test" else "input"
+day = os.path.basename(__file__).split("y")[1].split("_")[0]
+input_file = open(f"day{day}_{input}.txt")
+
+lines = [line.strip() for line in input_file]
+
+ranges = []
+ingredients = []
+reading_ranges = True
+count = 0
+
+for line in lines:
+  if line == "":
+    reading_ranges = False
+    continue
+  if reading_ranges:
+    start, end = line.split("-")
+    ranges.append((int(start), int(end)+1))
+    continue
+  
+  ingredients.append(int(line))
+  
+for ingredient in ingredients:
+  for start, end in ranges:
+    if start <= ingredient < end:
+      count += 1
+      break
+
+print("Part 1 Answer:", count)
+
+# Realized after that my solution is wrong, but it still passed on my input lol
+# I only deconflicted neighboring ranges.
+count = 0
+ranges = sorted(ranges, key=lambda x: (x[1], x[0]))
+print(ranges)
+
+for i in range(len(ranges)):
+  start, end = ranges[i]
+  count += (end - start)
+  print(count)
+
+  if i < len(ranges) - 1:
+    nextStart, nextEnd = ranges[i+1]
+    if nextStart < end:
+      count -= min(end, nextEnd) - max(start, nextStart)
+      print(count)
+
+print("Part 2 Answer:", count)
--- 2025/run_tests.py
+++ 2025/run_tests.py
@@ -31,7 +31,7 @@ for line in input_file.readlines():
       else:
         test_out[part][case]["debug"] += output_line + "\n"
   # Next test case
-  elif line.startswith("\n"):
+  elif line.startswith("---"):
     output_file = open(f"{file_prefix}_testcase.txt", "w")
     case += 1
   # Write test case
